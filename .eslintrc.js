267238a51fc03baaae2307249f1c908ce145441e
wangshunnn <wangshunnn@gmail.com>
2025-03-30 13:19:45 +0800

    chore: update

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {
-    semi: [2, 'always'],
+    semi: 0,
     '@typescript-eslint/no-unused-vars': 0,
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/explicit-module-boundary-types': 0,
