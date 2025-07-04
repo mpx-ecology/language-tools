b8017994ca4b800eb01bf146fbc35b26fe384bbf
Soon Wang <wangshunnn@gmail.com>
2025-07-04 12:01:26 +0800

    feat(inspect): update cases for script-json

--- inspect-extension/components/script-json/json-js/01.js
+++
@@ -1,9 +0,0 @@
-const wxConfig = { component: true, usingComponents: {} }
-const aliConfig = { component: true, usingComponents: {} }
-let finalConfig = null
-if (__mpx_mode__ === 'ali') {
-  finalConfig = aliConfig1
-} else {
-  finalConfig = wxConfig
-}
-module.exports = finalConfig
