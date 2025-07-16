4479a50316969b6a621deab07d49119ae419c536
Soon Wang <wangshunnn@gmail.com>
2025-07-16 10:51:21 +0800

    feat(service): update json schema

--- packages/language-service/src/data/mpxJsonSchema.ts
+++ packages/language-service/src/data/mpxJsonSchema.ts
@@ -51,24 +51,57 @@ export default {
       default: '#ffffff',
       markdownDescription: `页面背景颜色，如 #000000。\n\n详见：${WxDocsWithUrl}`,
     },
+    backgroundColorContent: {
+      type: 'string',
+      default: '#RRGGBBAA',
+      markdownDescription: `页面容器背景色。\n\n详见：${WxDocsWithUrl}`,
+    },
     backgroundTextStyle: {
       type: 'string',
       enum: ['dark', 'light'],
       default: 'dark',
       markdownDescription: `下拉 loading 的样式，仅支持 dark / light。\n\n详见：${WxDocsWithUrl}`,
     },
+    backgroundColorTop: {
+      type: 'string',
+      default: '#ffffff',
+      markdownDescription: `顶部窗口的背景色，仅 iOS 支持。\n\n详见：${WxDocsWithUrl}`,
+    },
+    backgroundColorBottom: {
+      type: 'string',
+      default: '#ffffff',
+      markdownDescription: `底部窗口的背景色，仅 iOS 支持。\n\n详见：${WxDocsWithUrl}`,
+    },
+    enablePullDownRefresh: {
+      type: 'boolean',
+      default: false,
+      markdownDescription: `是否开启当前页面下拉刷新，默认 false。\n\n详见：${WxDocsWithUrl}`,
+    },
+    pageOrientation: {
+      type: 'string',
+      enum: ['auto', 'portrait', 'landscape'],
+      default: 'portrait',
+      markdownDescription: `屏幕旋转设置，仅支持：\n\n- auto 自动\n- portrait 竖屏，默认值\n- landscape 横屏\n\n详见：${WxDocsWithUrl}`,
+    },
     disableScroll: {
       type: 'boolean',
       default: false,
       markdownDescription: `设置为 true 则页面整体不能上下滚动。只在页面配置中有效，无法在 app.json 中设置。\n\n详见：${WxDocsWithUrl}`,
     },
+    restartStrategy: {
+      type: 'string',
+      default: 'homePage',
+      markdownDescription: `重新启动策略配置，默认 homePage。\n\n详见：${WxDocsWithUrl}`,
+    },
     renderer: {
       type: 'string',
+      enum: ['webview', 'skyline', 'xr-frame'],
+      default: 'webview',
       markdownDescription: `渲染后端。\n\n详见：${WxDocsWithUrl}`,
     },
     componentFramework: {
       type: 'string',
-      markdownDescription: `组件框架。\n\n详见：[${WxDocsWithUrl}](${WxDocsUrl}/framework/custom-component/glass-easel/migration.html#JSON-配置)`,
+      markdownDescription: `组件框架。\n\n详见：[${WxDocs}](${WxDocsUrl}/framework/custom-component/glass-easel/migration.html#JSON-配置)`,
     },
   },
 }
